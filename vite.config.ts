a0306c72d42df84fb4bb3484a76b688fb7570231
CassiaPerez <cassia.perez@grupocropfield.com.br>
2025-08-25 14:44:40 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   // Altere conforme o ambiente:
-  // - Dev local: http://127.0.0.1:3006
+  // - Dev local: http://127.0.0.1:3005
   // - StackBlitz/Remoto: https://intranet.grupocropfield.com.br
-  const target = env.VITE_API_URL || 'http://127.0.0.1:3006'
+  const target = env.VITE_API_URL || 'http://127.0.0.1:3005'
 
   return {
     plugins: [react()],
